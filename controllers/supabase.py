04e7795f72419a2455b0d8946cb188fddd252bb9
ojnim <seungmincho03@gmail.com>
2024-10-22 22:38:01 -0400

    supabase structure setup

--- controllers/supabase.py
+++ controllers/supabase.py
@@ -6,19 +6,38 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 #fetch
-response = supabase.table("countries").select("*").execute()
+def get_supabase_data():
+    response = supabase.table("countries").select("*").execute()
+    return response
 
-#insert format
+#insert data
+def insert_supabase_data(d):
+    try:
+        data = supabase.table("summaries").insert(d).execute()
+        assert len(data.data) > 0
+        return data.data
+    except Exception as e:
+        return {"error": str(e)}
+
+
+"""
+insert format
 response = (
     supabase.table("countries")
     .insert({"id": 1, "name": "Denmark"})
     .execute()
 )
+"""
+def data_update(d_update):
+    #d_update dictionary forat
+    data = supabase.table("countries").update(d_update).eq("id", 1).execute()
 
-#update
+"""
+update format
 response = (
     supabase.table("countries")
     .update({"name": "Australia"})
     .eq("id", 1)
     .execute()
 )
+"""
