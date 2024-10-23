2e786e882c2d845e4207db45059ea0d1d0bd2c6b
ojnim <seungmincho03@gmail.com>
2024-10-22 22:39:04 -0400

    functions file path change & databse connection

--- controllers/functions.py
+++ controllers/functions.py
@@ -1,30 +1,36 @@
-import os
 from flask import Flask, request, jsonify
 
 # fix package imports
-from video import process_video
-from summarize import dialogue_processing
-from audio import process_audio
+from services.video import process_video
+from services.summarize import dialogue_processing
+from services.audio import process_audio
+from services.score_calculation import score
+from supabase import insert_supabase_data
+from dailyDB import get_dailydb_data
 
+def interview_score():
+    try:
+        interview_score = score() #interview result. would call function from services file
+        result = insert_supabase_data({"interview_score": interview_score})
 
+        return jsonify({'result': result}), 200
+    
+    except Exception as e:
+         return jsonify({'error': str(e)}), 500
 
-def hello_world():
-    return "<p>Hello, World!</p>"
-
+    return None
 
 def text_summarization():
     try:
         #get data from DailyDB
-        data = request.get_json()
-        if not data or 'dialogue' not in data:
-            return jsonify({'error': 'Invalid input data'}), 400
-        dialogue = data['dialogue']
+        dialogue = get_dailydb_data()
         
         # Summarization
         summarized_dialogue = dialogue_processing(dialogue)
+        result = insert_supabase_data({"summarized_dialogue": summarized_dialogue})
         
         # Return summarized data to superbase
-        return jsonify({'summarized_dialogue': summarized_dialogue}), 200
+        return jsonify({'result': result}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
