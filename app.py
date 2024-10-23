89f9d50dd9c335d34bf564a86f46c5b15a4db3d9
ojnim <seungmincho03@gmail.com>
2024-10-22 22:37:18 -0400

    app.py organize and add score_calculation.py file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify
-from summarize import dialogue_processing
+from services.summarize import dialogue_processing
 
 
 '''
@@ -16,34 +16,18 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
+#call the functions from controllers file
+#to be updated
 @app.route("/url", methods=['GET','POST'])
 def text_summarization():
-    try:
-        #get data from DailyDB
-        data = request.get_json()
-        if not data or 'dialogue' not in data:
-            return jsonify({'error': 'Invalid input data'}), 400
-        dialogue = data['dialogue']
-        
-        # Summarization
-        summarized_dialogue = dialogue_processing(dialogue)
-        
-        # Return summarized data to superbase
-        return jsonify({'summarized_dialogue': summarized_dialogue}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
+    pass
 
 @app.route("/url", methods=['GET','POST'])
 def video_summarization():
-
-    return None
-
+    pass
 @app.route("/url", methods=['GET','POST'])
 def audio_summarization():
-    
-    return None
+    pass
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
---
+++ services/score_calculation.py
@@ -0,0 +1,9 @@
+import video
+import audio
+import summarize
+
+def score():
+    #input: 
+    #output: dictionary of scores (total, individual scores from different eval method)
+    score_dict = {}
+    return score_dict
