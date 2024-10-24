4d8f4121befdf0488ff8e6fcf2be0b791e3dbd7a
ojnim <seungmincho03@gmail.com>
2024-10-24 12:12:33 -0400

    score calculation basic structure

--- services/score_calculation.py
+++ services/score_calculation.py
@@ -5,5 +5,10 @@ import summarize
 def score():
     #input: 
     #output: dictionary of scores (total, individual scores from different eval method)
-    score_dict = {}
+    total = 0
+    text = 0
+    audio = 0
+    video = 0
+    #exact variable needs to be discussed
+    score_dict = {"total":total,"text":text,"audio":audio,"video":video}
     return score_dict
