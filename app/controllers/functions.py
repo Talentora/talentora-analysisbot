b5bf4bcb282acd0460f6d5a790e0f2fae7f520ef
Lucas <junsuny789@gmail.com>
2024-11-01 19:03:59 -0400

    yes5

--- app/controllers/functions.py
+++ app/controllers/functions.py
@@ -24,21 +24,6 @@ def interview_evaluation():
     #      return jsonify({'error': str(e)}), 500
 
 
-def text_summarization():
-    try:
-        #get data from DailyDB
-        dialogue = get_dailydb_data()
-        
-        # Summarization
-        summarized_dialogue = dialogue_processing(dialogue)
-        result = supabase_client.insert_supabase_data({"summarized_dialogue": summarized_dialogue})
-        
-        # Return summarized data to superbase
-        return jsonify({'result': result}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
 def video_summarization():
     pass
     # analyze()
--- app/rotes/summarize.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-from ..utils import *
-from flask_cors import cross_origin, CORS
-from app.services import *
-
-
-bp = Blueprint('summarize', __name__)
-CORS(bp)
