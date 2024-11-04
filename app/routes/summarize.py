ab7002775547a70dfa9f7a2827b79239f493fbcc
Lucas <junsuny789@gmail.com>
2024-11-03 22:49:44 -0500

    woohoo

--- app/routes/summarize.py
+++ app/routes/summarize.py
@@ -7,7 +7,7 @@ from app.services.summarize import dialogue_processing
 from app.services.audio import process_audio
 # from app.services.sentiment_analysis import analyze
 
-supabase_client = supabase()
+# supabase_client = supabase()
 bp = Blueprint('summarize', __name__)
 CORS(bp)
 
@@ -17,19 +17,20 @@ CORS(bp)
 @bp.route("/url", methods=['GET','POST'])
 @cross_origin
 def interview_response_summarization():
-    def text_summarization():
-        try:
-            #get data from DailyDB
-            # dialogue = get_dailydb_data()
-            dialogue = ""
-            # Summarization
-            summarized_dialogue = dialogue_processing(dialogue)
-            result = supabase_client.insert_supabase_data({"summarized_dialogue": summarized_dialogue})
+    pass
+    # def text_summarization():
+    #     try:
+    #         #get data from DailyDB
+    #         # dialogue = get_dailydb_data()
+    #         dialogue = ""
+    #         # Summarization
+    #         summarized_dialogue = dialogue_processing(dialogue)
+    #         result = supabase_client.insert_supabase_data({"summarized_dialogue": summarized_dialogue})
             
-            # Return summarized data to superbase
-            return handle_success(result)
-        except Exception as e:
-            return handle_server_error(e)
+    #         # Return summarized data to superbase
+    #         return handle_success(result)
+    #     except Exception as e:
+    #         return handle_server_error(e)
 
 
 @bp.route("/url", methods=['GET','POST'])
