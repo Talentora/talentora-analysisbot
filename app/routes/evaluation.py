53ea163826909445cc1072a65f9c20baf16d54aa
ojnim <seungmincho03@gmail.com>
2024-11-10 11:32:35 -0500

    call functions

--- app/routes/evaluation.py
+++ app/routes/evaluation.py
@@ -13,11 +13,11 @@ CORS(bp)
 @cross_origin
 def interview_evaluation():
     try:
-        #get text_raw
-        #get interview questions
-        #get min_qual
-        #get preferred_qual
-        #get table to store
+        text_raw = get_dailydb_data()
+        questions = get_supabase_data()
+        min_qual = get_supabase_data()
+        preferred_qual = get_supabase_data()
+        table = get_supabase_data()
 
         #{"total":total,"text":lex,"audio":audio,"video":video}
         interview_eval = score_calculation.eval_result(text_raw, questions, min_qual, preferred_qual)
