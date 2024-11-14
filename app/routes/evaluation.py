5cb24bcb912a7fad705e3d5b2cee845a28c09f30
ojnim <seungmincho03@gmail.com>
2024-11-14 18:55:14 -0500

    supabase query

--- app/routes/evaluation.py
+++ app/routes/evaluation.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin, CORS
 from ..utils import *
 from app.services import score_calculation
-from controllers.supabase_db import insert_supabase_data, get_supabase_data
+from controllers.supabase_db import insert_supabase_data, get_supabase_data, update_supabase_data
 from app.controllers.daily_db import get_dailydb_data
 
 
@@ -14,17 +14,25 @@ CORS(bp)
 def interview_evaluation():
     try:
         text_raw = get_dailydb_data()
-        questions = get_supabase_data()
-        min_qual = get_supabase_data()
-        preferred_qual = get_supabase_data()
-        table = get_supabase_data()
+        #job_interview_config table interview_questions column
+        job_id = ""
+        applicant_id = ""
+
+        get_conditions = ["job_id",job_id]
+        questions = get_supabase_data("job_interview_config","interview_questions",get_conditions)
+        #job_interview_config table 
+        min_qual = get_supabase_data("job_interview_config","column_name",get_conditions)
+        #job_interview_config table 
+        preferred_qual = get_supabase_data("job_interview_config","column_name",get_conditions)
 
         #{"total":total,"text":lex,"audio":audio,"video":video}
         interview_eval = score_calculation.eval_result(text_raw, questions, min_qual, preferred_qual)
 
         #send evaluation
-        result = insert_supabase_data(table,interview_eval)
+        update_conditions = ["applicant_id",applicant_id]
+        data = {'interview_eval':interview_eval}
+        result = update_supabase_data("applicants",data, update_conditions)
 
         return handle_success(result)
     except Exception as e:
-         return handle_server_error(e)
+        return handle_server_error(e)
