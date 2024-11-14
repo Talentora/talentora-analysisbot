2fefde0d9e6adf578601f0e55d4b92784d8d95bd
ojnim <seungmincho03@gmail.com>
2024-11-14 00:23:04 -0500

    update

--- app/routes/summarize.py
+++ app/routes/summarize.py
@@ -2,9 +2,10 @@ from flask import Blueprint
 from ..utils import *
 from flask_cors import cross_origin, CORS
 import app.services.supabase as supabase
-# from dailyDB import get_dailydb_data
 from app.services.summarize import dialogue_processing
 from app.services.audio import process_audio
+from app.controllers.daily_db import get_dailydb_data
+from controllers.supabase_db import insert_supabase_data
 # from app.services.sentiment_analysis import analyze
 
 supabase_client = supabase()
@@ -12,24 +13,20 @@ bp = Blueprint('summarize', __name__)
 CORS(bp)
 
 
-#call the functions from controllers file
-#to be updated
 @bp.route("/url", methods=['GET','POST'])
 @cross_origin
 def interview_response_summarization():
-    def text_summarization():
-        try:
-            #get data from DailyDB
-            # dialogue = get_dailydb_data()
-            dialogue = ""
-            # Summarization
-            summarized_dialogue = dialogue_processing(dialogue)
-            result = supabase_client.insert_supabase_data({"summarized_dialogue": summarized_dialogue})
+    try:
+        #get data from DailyDB
+        dialogue = get_dailydb_data()
+        # Summarization
+        summarized_dialogue = dialogue_processing(dialogue)
+        result = supabase_client.insert_supabase_data({"summarized_dialogue": summarized_dialogue})
             
-            # Return summarized data to superbase
-            return handle_success(result)
-        except Exception as e:
-            return handle_server_error(e)
+        # Return summarized data to superbase
+        return handle_success(result)
+    except Exception as e:
+        return handle_server_error(e)
 
 
 @bp.route("/url", methods=['GET','POST'])
