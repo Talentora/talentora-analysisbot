049bd5b9b899c7924dd3e68b733ba17eaffb8df4
Lucas <junsuny789@gmail.com>
2024-11-17 14:05:38 -0500

    commendted out problematic dailydb code

--- app/controllers/daily_db.py
+++ app/controllers/daily_db.py
@@ -1,23 +1,23 @@
-import os
-from supabase import create_client, Client
+# import os
+# from supabase import create_client, Client
 
-url: str = os.environ.get("DAILY_URL")
-key: str = os.environ.get("DAILY_KEY")
-supabase: Client = create_client(url, key)
+# url: str = os.environ.get("DAILY_URL")
+# key: str = os.environ.get("DAILY_KEY")
+# supabase: Client = create_client(url, key)
 
-#fetch
-def get_dailydb_data(table,select_target):
-    pass
+# #fetch
+# def get_dailydb_data(table,select_target):
+#     pass
 
-#insert data
-def insert_dailydb_data(table, data_for_insert):
-    pass
+# #insert data
+# def insert_dailydb_data(table, data_for_insert):
+#     pass
 
-def data_insert_format_dailydb(table, data_for_insert):
-    pass
+# def data_insert_format_dailydb(table, data_for_insert):
+#     pass
 
-def update_dailydb_data(table_name,data_for_update,condition):
-    pass
+# def update_dailydb_data(table_name,data_for_update,condition):
+#     pass
 
-def data_update_format_dailydb(table_name,data_for_update,condition):
-    pass
+# def data_update_format_dailydb(table_name,data_for_update,condition):
+#     pass
