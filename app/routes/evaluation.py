fb762d12d3a96a6d2de811ad39383b3a94fc9919
Vincent Li <vincentli0921@gmail.com>
2024-12-04 02:24:45 -0500

    hotfix

--- app/routes/evaluation.py
+++ app/routes/evaluation.py
@@ -1,38 +1,26 @@
 from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin, CORS
-import os
 import hmac
 import base64
 import hashlib
-import uuid
-from dotenv import load_dotenv
+from app.configs.daily_config import DAILY_API_KEY, DAILY_WEBHOOK_SECRET
 from ..utils import *
 from app.services import score_calculation
 from app.services import summarize
 from app.services.sentiment import run_emotion_analysis
-from app.controllers.supabase_db import SupabaseDB
 from app.controllers.dailybatchprocessor import DailyBatchProcessor
 from app.controllers.recording_link import DailyVideoDownloader
-
-load_dotenv()
-
-api_key=os.environ.get("DAILY_API_KEY")
-webhook_secret = os.environ.get("DAILY_WEBHOOK_SECRET")
-
-if not api_key:
-    # raise EnvironmentError("DAILY_API_KEY environment variable is not set!")
-    api_key = "none"
 
 bp = Blueprint('eval', __name__)
 CORS(bp)
 
 def verify_webhook_signature(timestamp, raw_body, signature):
-    if not webhook_secret:
+    if not DAILY_WEBHOOK_SECRET:
         return True  # Skip verification if secret not set
         
     try:
         # Decode the base64 secret
-        decoded_secret = base64.b64decode(webhook_secret)
+        decoded_secret = base64.b64decode(DAILY_WEBHOOK_SECRET)
         
         # Create signature string
         signature_string = f"{timestamp}.{raw_body}"
@@ -68,9 +56,8 @@ def handle_webhook():
                 return jsonify({'error': 'Invalid signature'}), 401
         
         
-        batch_processor = DailyBatchProcessor(api_key)
-        downloader = DailyVideoDownloader(api_key)
-        database = SupabaseDB()
+        batch_processor = DailyBatchProcessor(DAILY_API_KEY)
+        downloader = DailyVideoDownloader(DAILY_API_KEY)
 
         # Parse JSON data
         data = request.get_json()
@@ -101,10 +88,6 @@ def handle_webhook():
             job_id = data['payload']['id']
             
             text_raw = batch_processor.process_transcription_job(job_id)
-            summary = batch_processor.process_summary_job(job_id)
-            
-            summary_data = {"interview_summary":summary}
-            database.insert_supabase_data("AI_summary", summary_data)
             
             recording_id = "cf6bcc01-14ac-48d5-9473-bbc516522e1c"
             
