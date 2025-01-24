b6844d6edf01c5fbb8ad8b65c43f9d0b2718a4f9
Lucas <junsuny789@gmail.com>
2025-01-24 14:51:51 -0500

    updating readme.md and adding heartbeat

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from .evaluation import bp as evaluation_bp
 from .hume_callback import bp_hume as hume_callback_bp
+from app.utils import handle_success
 
 """A function to register all the blueprints."""
 def register_blueprints(app: Flask):
     app.register_blueprint(evaluation_bp, url_prefix='/evaluation')
     app.register_blueprint(hume_callback_bp, url_prefix='/hume-callback')
     
+    @app.route('/', methods=['GET'])
+    def heartbeat():
+        return handle_success("It's running")
