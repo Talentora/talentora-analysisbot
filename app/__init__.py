c4fb6a269b22f0dbf89f497131719a3e77a1a97a
Lucas <junsuny789@gmail.com>
2025-05-08 15:19:16 -0400

    merge webhook

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.routes.resume_routes import resume_bp  # Import the resume routes
 from app.routes.health import health_bp  # Import the health routes
 from app.routes.evaluation import bp as evaluation_bp
 from app.routes.hume_callback import bp_hume as hume_callback_bp
-
+from app.routes import register_blueprints
 def create_app(Test=False) -> Flask:
     app = Flask(__name__)
     app.config["TESTING"] = Test
@@ -13,10 +13,12 @@ def create_app(Test=False) -> Flask:
     CORS(app, resources={r"/*": {"origins": "*"}})
 
     # Register the blueprints
-    app.register_blueprint(resume_bp)
-    app.register_blueprint(health_bp)  # Register the health blueprint
-    app.register_blueprint(evaluation_bp, url_prefix="/evaluation")
-    app.register_blueprint(hume_callback_bp, url_prefix="/hume-callback")
+    # app.register_blueprint(resume_bp)
+    # app.register_blueprint(health_bp)  # Register the health blueprint
+    # app.register_blueprint(evaluation_bp, url_prefix="/evaluation")
+    # app.register_blueprint(hume_callback_bp, url_prefix="/hume-callback")
+    # app.register_blueprint(merge_bp, url_prefix="/merge")
+    register_blueprints(app)
 
     return app
 
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from .evaluation import bp as evaluation_bp
 from .hume_callback import bp_hume as hume_callback_bp
+from .merge_routes import merge_bp as merge_bp
 from app.utils import handle_success
 
 """A function to register all the blueprints."""
 def register_blueprints(app: Flask):
     app.register_blueprint(evaluation_bp, url_prefix='/evaluation')
     app.register_blueprint(hume_callback_bp, url_prefix='/hume-callback')
+    app.register_blueprint(merge_bp, url_prefix='/merge')
     
     @app.route('/', methods=['GET'])
     def heartbeat():
